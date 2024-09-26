723616daa4c4a328cb6dd4eb2b0f5fe2713bb59d
Muzamil3 <sp24-bse-077@cuilahore.edu.pk>
2024-09-26 17:32:31 +0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Shop shop= new Shop(new Person("John", "email"),"Lahore");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int choice= sc.nextInt();
+            switch (choice) {
+                case 1:
+                shop.addProduct();
+                break;
+                case 2:
+                    shop.sale();
+                    break;
+case 3:
+shop.Edit();
+break;
+case 4:
+shop.Display();
+break;
+                case 0:
+                   System.exit(0);
+                   break;
+                default:
+            }
+
+
+
+        }
+
+
+
+    }
+}
